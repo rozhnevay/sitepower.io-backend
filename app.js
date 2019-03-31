413b6737c4af29b6d3dd8b5a700da16ccc935d8e
rozhnevay <weic8vne>
2019-03-31 15:56:29 +0300

    #8

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
   store: sessionStore,
   secret: process.env.SECRET,
-  key: 'sitepower.sid',
+  key: 'sitepower.sid.' + process.env.APP_MODE,
   resave: false,
   saveUninitialized: true,
   cookie: {
