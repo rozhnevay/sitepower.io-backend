d273910a29ba47dc2975977bfc51ec694c6578d5
Alexey Rozhnev <Rozhnev.A.Yur@sberbank.ru>
2019-12-03 14:39:56 +0300

    bitrix commit

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ require('./api-upload')(app, authMiddleware); // Upload
 require('./api-vk')(app, authMiddleware); // vk-api
 require('./api-alice')(app, authMiddleware); // alice-api
 require('./api-oratorart')(app); // alice-api
+require('./api-bitrix')(app); // alice-api
 
  // Socket API
 require('./api-chat')(app, authMiddleware); // Chat API
