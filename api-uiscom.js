ab046dd267cd515adf3df25ee2f25c52d9fa6917
Aleksey Rozhnev <rozhnevay@gmail.com>
2022-07-27 17:16:09 +0300

    uiscom added

--- api-uiscom.js
+++ api-uiscom.js
@@ -7,25 +7,25 @@ module.exports = function (app) {
         db.createRequestLog(JSON.stringify(req));
     }
 
-    app.post("/api/mango/events/call/waiting", (req, res) => {
+    app.post("/api/uiscom/events/call/waiting", (req, res) => {
         console.log("!!! waiting !!!");
         console.log(JSON.parse(req.body));
         res.send("OK");
     });
 
-    app.post("/api/mango/events/call/connected", (req, res) => {
+    app.post("/api/uiscom/events/call/connected", (req, res) => {
         console.log("!!! connected !!!");
         console.log(JSON.parse(req.body));
         res.send("OK");
     });
 
-    app.post("/api/mango/events/call/disconnected", (req, res) => {
+    app.post("/api/uiscom/events/call/disconnected", (req, res) => {
         console.log("!!! disconnected !!!");
         console.log(JSON.parse(req.body));
         res.send("OK");
     });
 
-    app.post("/api/mango/events/call/missed", (req, res) => {
+    app.post("/api/uiscom/events/call/missed", (req, res) => {
         console.log("!!! missed !!!");
         console.log(JSON.parse(req.body));
         res.send("OK");
